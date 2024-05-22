da411dd9175e5da69ca29a48390fe5abc4e3eb27
RogersKaran <karanjarodgers@gmail.com>
2024-05-22 03:02:46 -0700

    adjusting files to integrate socket.io

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const http = require('http');
+const { Server } = require('socket.io');
+const { join, dirname } = require('path');
+const { fileURLToPath } = require('url');
 
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server);
 
 app.get('/', (req, res) => {
-	  res.send('<h1>Hello world</h1>');
+  res.sendFile(join(__dirname, 'index.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
 
 server.listen(3000, () => {
-	  console.log('server running at http://localhost:3000');
+  console.log('server running at http://localhost:3000');
 });
 
